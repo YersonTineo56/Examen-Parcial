a89ca78a1eddf91173e3c2d9abaea42eebc2dee3
YersonTineo56 <150179944+YersonTineo56@users.noreply.github.com>
2024-11-19 11:49:54 -0500

    Eliminar todos los archivos en main

--- src/main/java/com/example/examen_parcial/util/Routes.java
+++
@@ -1,9 +0,0 @@
-package com.example.examen_parcial.util;
-
-public class Routes {
-    public static final String API_ALUMNOS = "/api/alumnos";
-    public static final String ALUMNO_ID = "/{id}";
-    
-    public static final String API_CURSOS = "/api/cursos";
-    public static final String CURSO_ID = "/{id}";
-}
